0ec2798948ce92f4a5ed5bca7da415f075994c3d
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-09-09 14:51:32 -0700

    Add DB context

---
+++ AnimalCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using AnimalCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalCatalogAPI.Data
+{
+    // database context class, provides instruction to entity framework core 
+    // when it is about to set the DB
+    public class CatalogContext:DbContext
+    {
+        // dependency injection -> in constructor
+        public CatalogContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<CatalogBreed> CatalogBreeds { get; set; }
+        public DbSet<CatalogType> Catalogtypes { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+    }
+}
